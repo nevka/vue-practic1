b6350aae73bfca17c48a34b23311ea9631b45e43
Алина <nevkaa@yandex.ru>
2018-12-01 11:22:47 +0300

    4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,71 @@
 <template>
-  <h1>Hello</h1>
+  <v-app>
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list>
+        <v-list-tile v-for="link of links" :key="link.title" :to="link.url">
+          <v-list-tile-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app dark color="primary">
+      <v-toolbar-side-icon @click="drawer =! drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Добавить приложение</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat v-for="link of links" :key="link.title" :to="link.url">
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+
+      <router-view></router-view>
+
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {
+          title: 'Вход',
+          icon: 'lock',
+          url: '/login'
+        },
+        {
+          title: 'Регистрация',
+          icon: 'face',
+          url: '/registration'
+        },
+        {
+          title: 'Заказы',
+          icon: 'bookmark_border',
+          url: '/orders'
+        },
+        {
+          title: 'Добавить',
+          icon: 'note_add',
+          url: '/new'
+        },
+        {
+          title: 'Мои заказы',
+          icon: 'list',
+          url: '/list'
+        }
+      ]
+    }
+  }
+}
 </script>
