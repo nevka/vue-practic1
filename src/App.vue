0f8632f7ebc8b6898fc874adb31acf5a27d9ec9a
Алина <nevkaa@yandex.ru>
2018-12-01 14:17:25 +0300

    8

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,9 @@
 
     <v-toolbar app dark color="primary">
       <v-toolbar-side-icon @click="drawer =! drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>Добавить приложение</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" class="pointer">Добавить приложение</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn flat v-for="link of links" :key="link.title" :to="link.url">
@@ -69,3 +71,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .pointer{
+    cursor: pointer;
+  }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,82 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Home</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div>
+    <v-container fluid>
+      <v-layout row>
+        <v-flex xs12>
+          <v-carousel>
+            <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.imageSrc">
+              <div class="car-link">
+                <v-btn class="error" :to="'/add/' + ad.id">{{ad.title}}</v-btn>
+              </div>
+            </v-carousel-item>
+          </v-carousel>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 v-for="ad in ads" :key="ad.id">
+          <v-card>
+            <v-img :src="ad.imageSrc" aspect-ratio="2.75"></v-img>
+            <v-card-title primary-title>
+              <div><h3 class="headline mb-0">{{ad.title}}</h3>
+                <div>{{ad.description}}</div>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat :to="'/add/' + ad.id">Открыть</v-btn>
+              <v-btn flat raised class="primary">Купить</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
-      return {}
+      return {
+        ads: [
+          {
+            title: 'First ad',
+            description: 'Hello i am description',
+            promo: false,
+            imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+            id: '123'
+          },
+          {
+            title: 'Second ad',
+            description: 'Hello i am description',
+            promo: true,
+            imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            id: '1234'
+          },
+          {
+            title: 'Third ad',
+            description: 'Hello i am description',
+            promo: true,
+            imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            id: '12345'
+          }
+        ]
+      }
     }
   }
 </script>
 
 <style scoped>
-
+  .car-link {
+    position: absolute;
+    bottom: 50px;
+    left: 50%;
+    background: rgba(0, 0, 0, .5);
+    transform: translate(-50%, 0);
+    padding: 5px 15px;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+  }
 </style>
