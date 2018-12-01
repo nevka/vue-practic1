7fb19a2b632bef71c6b90b954772baa7baef4dac
Алина <nevkaa@yandex.ru>
2018-12-01 14:56:24 +0300

    10

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,36 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>NewAd</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Создать новую заявку</h1>
+        <v-form class="mb-3" v-model="valid" ref="form" validation>
+          <v-text-field name="title" label="Название" type="text" required v-model="title" :rules="[v => !!v || 'Название обязательно для заполнения']"></v-text-field>
+          <v-text-field name="description" label="Описание" type="text" multi-line v-model="description" :rules="[v => !!v || 'Описание обязательно для заполнения']"></v-text-field>
+        </v-form>
+        <v-layout row class="mb-3">
+          <v-flex xs12>
+            <v-btn class="warning">
+              Upload
+              <v-icon right dark>cloud_upload</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <img src="" height="100">
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch color="primary" label="Добавить в промо?" v-model="promo"></v-switch>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-spacer></v-spacer>
+            <v-btn class="success" @click="createAd" :disabled="!valid">Добавить заявку</v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +39,25 @@
 <script>
   export default {
     data () {
-      return {}
+      return {
+        title: '',
+        description: '',
+        promo: false,
+        valid: false
+      }
+    },
+    methods: {
+      createAd () {
+        if (this.$refs.form.validate()) {
+          const ad = {
+            title: this.title,
+            description: this.description,
+            promo: this.promo
+          }
+
+          console.log(ad)
+        }
+      }
     }
   }
 </script>
